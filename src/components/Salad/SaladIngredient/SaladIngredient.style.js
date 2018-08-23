f1cf21928eb76815e1567b1008adf830238ec0bd
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-22 21:54:25 -0500

    Change Ingredients styles

--- src/components/Salad/SaladIngredient/SaladIngredient.style.js
+++ src/components/Salad/SaladIngredient/SaladIngredient.style.js
@@ -4,15 +4,16 @@ export const SaladBed = styled.div`
 	height: 13%;
  	width: 80%;
  	background: linear-gradient(#5bec65, #368c3c);
- 	border-radius: 0 0 30px 30px;
+ 	border-radius: 0 0 90px 90px;
  	box-shadow: inset -15px 0 #0e6514;
  	margin: 2% auto;
 `;
 
 export const Topping = styled.div`
-	width: 80%;
+				width: 80%;
     height: 3%;
-    background: linear-gradient(#bf3813, #815a07);
+				background: linear-gradient(#bf3813, #815a07);
+				border-radius: 30%;
     margin: 2% auto;
 `;
 
@@ -26,12 +27,12 @@ export const BerryContainer = styled.div`
 
 export const BlueBerry = styled.div`
 	width: 10%;
-	height: 15%;
+	height: 40%;
 	position: absolute;
 	background-color: #1c124d;
 	left: 30%;
-	top: 50%;
-	border-radius: 40%;
+	top: 64%;
+	border-radius: 57%;
 	transform: rotate(-20deg);
 	box-shadow: inset -2px -3px #140d36;
 	&:after {
@@ -42,7 +43,7 @@ export const BlueBerry = styled.div`
 		background-color: #1c124d;
 		left: -170%;
 		top: -260%;
-		border-radius: 40%;
+		border-radius: 57%;
 		transform: rotate(60deg);
 		box-shadow: inset -1px 2px #140d36;
 	}
@@ -54,31 +55,31 @@ export const BlueBerry = styled.div`
 		background-color: #1c124d;
 		left: 180%;
 		top: -50%;
-		border-radius: 40%;
+		border-radius: 57%;
 		transform: rotate(60deg);
 		box-shadow: inset -1px -3px #140d36;
 	}
 `;
 
 export const CranBerry = styled.div`
-	width: 10%;
-    height: 15%;
+				width: 10%;
+    height: 40%;
     position: absolute;
-    background-color: white;
+    background-color: #670318;
     left: 64%;
     top: 50%;
-    border-radius: 40%;
+    border-radius: 57%;
     transform: rotate(10deg);
-	box-shadow: inset -3px 0 #c9c9c9;
-	&:before {
-		content: "";
+				box-shadow: inset -3px 0 #38040f;
+				&:before {
+					content: "";
     	width: 100%;
     	height: 100%;
     	position: absolute;
     	background-color: #670318;
     	left: 150%;
     	top: -130%;
-    	border-radius: 40%;
+    	border-radius: 57%;
     	transform: rotate(90deg);
     	box-shadow: inset 1px 3px #38040f;
 	}
