c3b87ff419bb2495c31b9cc545fc59718a59fe47
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-22 20:30:45 -0500

    Create first styled component for SaladIngredients

--- src/components/Salad/SaladIngredient/SaladIngredient.style.js
+++ src/components/Salad/SaladIngredient/SaladIngredient.style.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export const SaladBed = styled.div`
- 
+	height: 13%;
+ 	width: 80%;
+ 	background: linear-gradient(#5bec65, #368c3c);
+ 	border-radius: 0 0 30px 30px;
+ 	box-shadow: inset -15px 0 #0e6514;
+ 	margin: 2% auto;
 `;
