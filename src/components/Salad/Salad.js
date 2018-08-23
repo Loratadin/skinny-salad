64a346e22c595610fedd15df8ede07d4920344ec
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-22 21:06:08 -0500

    Add Salad styled component

---
+++ src/components/Salad/Salad.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Salad } from './Salad.style.js';
+
+const salad = (props) => {
+ 	return (
+		<Salad/>
+	);
+};
+
+export default salad;
---
+++ src/components/Salad/SaladIngredient/Salad.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Salad = styled.div`
+	width: 100%;
+	margin: auto;
+	height: 250px;
+	overflow: scroll;
+	text-align: center;
+	font-size: 1.2rem;
+`;
