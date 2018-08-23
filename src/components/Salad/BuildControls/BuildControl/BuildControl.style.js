f0f7256ff1b78a44f955115250b83c6ffe2259ad
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-23 09:50:26 -0500

    Style buttons

---
+++ src/components/Salad/BuildControls/BuildControl/BuildControl.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const BuildControl = styled.div`
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ margin: 5px 0;
+`;
+export const BCButton = styled.btn`
+ display: block;
+ font: inherit;
+ padding: 5px;
+ margin: 0 5px;
+ width: 80px;
+ border: 1px solid #717015;
+ cursor: pointer;
+ outline: none;
+`;
