e876353e396cbb883216af8da1bbdf74bd365a14
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-23 09:27:21 -0500

    Style BuildControls

--- src/components/Salad/BuildControls/BuildControls.style.js
+++ src/components/Salad/BuildControls/BuildControls.style.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export const BuildControls = styled.div`
- 
+ width: 100%;
+ background-color: #f6f426;
+ display: flex;
+ flex-flow: column;
+ align-items: center;
+ box-shadow: 0 2px 2px #ccc;
+ margin: auto;
+ padding: 10px 0;
 `;
