e6664e3e7bb31590e6cdac577f0989289082d20c
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-23 05:55:30 -0500

    Output Salad Ingredients dynamically

--- src/components/Salad/Salad.js
+++ src/components/Salad/Salad.js
@@ -3,10 +3,16 @@ import { Salad } from './SaladIngredient/Salad.style';
 import SaladIngredient from './SaladIngredient/SaladIngredient';
 
 const salad = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <SaladIngredient key={igKey + i} type={igKey} />;
+			});
+		});
  	return (
 		<Salad>
 			<SaladIngredient type="topping" />
-			<SaladIngredient type="berry" />
+			{transformedIngredients}
 			<SaladIngredient type="salad-bed" />
 		</Salad>
 	);
