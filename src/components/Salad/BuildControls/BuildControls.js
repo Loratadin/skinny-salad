08d1dc7fbc5748dc5fe285575eadd367fb32adc5
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-23 10:10:42 -0500

    Import single BuildControl into BuildControls

--- src/components/Salad/BuildControls/BuildControls.js
+++ src/components/Salad/BuildControls/BuildControls.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { BuildControls } from './BuildControls.style';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+ { label: 'Berry', type: 'berry' },
+ { label: 'Topping', type: 'topping' },
+ { label: 'Herbs', type: 'herbs' },
+ { label: 'Seeds', type: 'seeds' }
+];
 
 const buildControls = (props) => {
  <BuildControls>
-
+  {controls.map(ctrl => (
+   <BuildControl key={ctrl.label} label={ctrl.label} />
+  ))}
  </BuildControls>
 };
 
