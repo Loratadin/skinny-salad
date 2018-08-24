190bd4f2a4fbf8abd3f2b94d27ab0e57b76cfca8
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-23 21:07:38 -0500

    Add addIngredient method

--- src/containers/SaladBuilder/SaladBuilder.js
+++ src/containers/SaladBuilder/SaladBuilder.js
@@ -3,6 +3,11 @@ import Aux from '../../hoc/Aux.js';
 import Salad from '../../components/Salad/Salad';
 import BuildControls from '../../components/Salad/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+	saladBed: 1,
+	berry: 0.7,
+	topping: 0.2
+}
 class SaladBuilder extends Component {
 
 	state = {
@@ -10,8 +15,26 @@ class SaladBuilder extends Component {
 			saladBed: 1,
 			topping: 1,
 			berry: 1
-		}
+		},
+		totalPrice: 3
 	}
+
+	addIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount + 1;
+		const updatedIngredients = {
+				...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+		const priceAddition = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + priceAddition;
+		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+	}
+	removeIngredientHandler = (type) => {
+		
+	}
+
  	render() {
 		 return (
 			<Aux>
