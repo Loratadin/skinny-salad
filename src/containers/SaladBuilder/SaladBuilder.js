0a6131a459d454c4bafc048c34616ba7df708f5b
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-23 21:20:23 -0500

    Set up removeIngredientHandler method

--- src/containers/SaladBuilder/SaladBuilder.js
+++ src/containers/SaladBuilder/SaladBuilder.js
@@ -32,7 +32,16 @@ class SaladBuilder extends Component {
 		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 	}
 	removeIngredientHandler = (type) => {
-		
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = {
+				...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - priceDeduction;
+		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 	}
 
  	render() {
