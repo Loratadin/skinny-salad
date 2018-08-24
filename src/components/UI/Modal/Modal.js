3143d4f9589de1634ca65b466720e523895d8b85
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-24 15:46:19 -0500

    Add inline styles for the Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Modal } from './Modal.style';
 
 const modal = (props) => (
- <Modal>
+ <Modal style={{
+  transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+  opacity: props.show ? '1' : '0',
+ }}>
   {props.children}
  </Modal>
 );
