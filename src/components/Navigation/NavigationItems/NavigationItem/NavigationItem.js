3bda2c4f3290fcd0613315da5f615e11dd12550c
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-25 13:32:11 -0500

    Import NavigationItem to items

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { NavigationItem } from './NavigationItem.style';
 
 const navigationItem = (props) => (
-    <NavigationItem><a href="/">A Link</a></NavigationItem>
+    <NavigationItem>
+        <a href={props.link}>{props.children}</a>
+    </NavigationItem>
 );
 
 export default navigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavigationItems } from './NavigationItems.style';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <NavigationItems>
-        
+        <NavigationItem link="/">Salad Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
     </NavigationItems>
 );
 
