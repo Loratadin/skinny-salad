62bb08d1cf4902f1ccb015f17c0e8b3c18e617aa
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-26 07:57:34 -0500

    Add Mobile SideDrawer to the Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import { Content } from './Layout.style.js';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar/>
+        <SideDrawer/>
         <Content>
             {props.children}
         </Content>
