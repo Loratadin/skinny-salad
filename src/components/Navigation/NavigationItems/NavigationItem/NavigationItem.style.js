157862bc70ce05de2480f2f881a43d1b87655c25
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-08-30 11:02:50 -0500

    Add media styles for NavigationItem

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.style.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.style.js
@@ -1,24 +1,34 @@
 import styled from 'styled-components';
+import media from '../../../../theme/media';
 
 export const NavigationItem = styled.div`
-    margin: 0; 
+    margin: 10px 0; 
     box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    height: 100%;
+    display: block;
+    width: 100%;
     & > a {
         text-decoration: none;
         color: white;
-        height: 100%;
-        padding: 16px 10px;
-        border-bottom: 4px solid transparent;
+        width: 100%;
         box-sizing: border-box;
         display: block;
     }
-    & > a:hover,
-    & > a:active,
-    & > a.active {
-        background-color: #b3b32a;
-        border-bottom: 4px solid #53c75c;
-    }
+    ${media.desktopL`
+        margin: 0; 
+        height: 100%;
+        width: auto;
+        display: flex;
+        align-items: center;
+        & > a {
+            height: 100%;
+            padding: 16px 10px;
+            border-bottom: 4px solid transparent;
+        }
+        & > a:hover,
+        & > a:active,
+        & > a.active {
+            background-color: #b3b32a;
+            border-bottom: 4px solid #53c75c;
+        }
+    `}
 `;
