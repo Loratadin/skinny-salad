39db343a85ee2a8c72391e4212e723f006600856
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-09-01 10:34:58 -0500

    Add sideDrawerClosed method

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import { Content } from './Layout.style.js';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <Content>
-            {props.children}
-        </Content>
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    render () {
+        return (
+            <Aux>
+                <Toolbar/>
+                <SideDrawer closed={this.sideDrawerClosedHandler} />
+                <Content>
+                    {this.props.children}
+                </Content>
     </Aux>
-);
+        )
+    }
+};
 
-export default layout;
+export default Layout;
