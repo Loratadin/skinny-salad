784b1b8bed06a7f8ac527b0122648f2eaf3a7444
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-10-29 16:59:48 -0500

    make Modal a stateful component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import { Modal } from "./Modal.style";
 import Aux from "../../../hoc/Aux";
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = ( props ) => (
-  <Aux>
-	  <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <Modal
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
+class Modal extends Component {
+  render () {
+    return (
+      <Aux>
+	      <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <Modal
+          style={{
+          transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: this.props.show ? "1" : "0"
+          }}
+        >
+        {this.props.children}
     </Modal>
   </Aux>
-);
+    );
+  }
+}
 
-export default modal;
+export default Modal;
