5b80dbf019162bfee42bcf1d537b47ad677e8615
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-11-09 17:15:07 -0600

    submit the order to the server

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Button from "../../../components/UI/Button/Button";
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import axios from '../../../axios-orders';
 import './ContactData.css';
 
 class ContactData extends Component {
@@ -10,20 +12,54 @@ class ContactData extends Component {
             street: ' ',
             zip: ' ',
             city: ' ',
-        }
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState( {loading: true} );
+		const order = {
+			ingredients: this.props.ingredients,
+			price: this.props.price,
+			customer: {
+				name: 'Tania Shulga',
+				address: {
+					street: 'TestStreet',
+					zipCode: '37287',
+					city: 'McKinney'
+				},
+				email: 'test@test.com'
+			},
+			deliveryMethod: 'fastest'
+		}
+		axios.post('/orders.json', order)
+			.then(response => {
+                this.setState( {loading: false} );
+                this.props.history.push('/');
+			})
+			.catch(error => {
+				this.setState( {loading: false} );
+			});
     }
     render() {
+        let form = (
+            <form>
+                <input className="Input" type="text" name="name" placeholder="Your name"/>
+                <input className="Input" type="email" name="email" placeholder="Your email"/>
+                <input className="Input" type="text" name="city" placeholder="Your city"/>
+                <input className="Input" type="text" name="street" placeholder="Your street"/>
+                <input className="Input" type="number" name="zip" placeholder="Your zip code"/>
+                <Button clicked={this.orderHandler}>ORDER</Button>
+                </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner/>;
+        }
         return (
             <div className="ContactData">
                 <h4>Enter your Contact Data</h4>
-                <form>
-                    <input className="Input" type="text" name="name" placeholder="Your name"/>
-                    <input className="Input" type="email" name="email" placeholder="Your email"/>
-                    <input className="Input" type="text" name="city" placeholder="Your city"/>
-                    <input className="Input" type="text" name="street" placeholder="Your street"/>
-                    <input className="Input" type="number" name="zip" placeholder="Your zip code"/>
-                    <Button>ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
