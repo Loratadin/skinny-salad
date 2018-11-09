97fe639d36490c62bdbcc8a5b310dce8e4a3abce
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-11-09 14:45:42 -0600

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import SaladBuilder from './containers/SaladBuilder/SaladBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -6,10 +7,12 @@ class App extends Component {
   render() {
     return (
       	<div>
-			<Layout>
-				<SaladBuilder/>
-				<Checkout />
-			</Layout>
+					<Layout>
+						<Switch>
+							<Route path="/checkout" component={Checkout} />
+							<Route path="/" exact component={SaladBuilder} />
+						</Switch>
+					</Layout>
       	</div>
     );
   }
