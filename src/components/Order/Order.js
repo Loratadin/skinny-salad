abe3c50f01c21dcd1a51da789d23df194f3da53c
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-11-12 09:22:43 -0600

    create Order and Orders components

---
+++ src/components/Order/Order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const order = (props) => (
+    <div>
+        <p>Ingredients: Salad (1)</p>
+        <p>Price: USD <strong>5.45</strong></p>
+    </div>
+);
+
+export default order;
